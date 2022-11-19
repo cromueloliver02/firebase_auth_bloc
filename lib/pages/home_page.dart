7c3741721cc58e56515f2c365707693ec68da20b
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-19 23:40:17 +0800

    home page widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:fb_auth_bloc/blocs/blocs.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,12 +6,27 @@ class HomePage extends StatelessWidget {
 
   const HomePage({super.key});
 
+  void _signout(BuildContext ctx) {
+    ctx.read<AuthBloc>().add(SignoutRequestedEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
-      child: const Scaffold(
-        body: Center(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+              onPressed: () => _signout(context),
+              iconSize: 30,
+              icon: const Icon(Icons.logout),
+            ),
+          ],
+        ),
+        body: const Center(
           child: Text('Home'),
         ),
       ),
