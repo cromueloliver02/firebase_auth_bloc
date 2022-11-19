ed8845254b1be4a068e188bdd2d79e88ce3db488
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-19 14:52:52 +0800

    signin cubit

---
+++ lib/cubits/signin/signin_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../models/custom_error.dart';
+import '../../repositories/auth_repository.dart';
+
+part 'signin_state.dart';
+
+class SigninCubit extends Cubit<SigninState> {
+  final AuthRepository authRepository;
+
+  SigninCubit({
+    required this.authRepository,
+  }) : super(SigninState.initial());
+
+  Future<void> signin({
+    required String email,
+    required String password,
+  }) async {
+    emit(state.copyWith(status: SigninStatus.submitting));
+
+    try {
+      await authRepository.signin(email: email, password: password);
+
+      emit(state.copyWith(status: SigninStatus.sucess));
+    } on CustomError catch (err) {
+      emit(state.copyWith(
+        status: SigninStatus.error,
+        error: err,
+      ));
+    }
+  }
+}
--- lib/utils/blocs_handler.dart
+++ lib/utils/blocs_handler.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../blocs/blocs.dart';
+import '../cubits/cubits.dart';
 import '../repositories/auth_repository.dart';
 
 class BlocsHandler {
@@ -16,6 +17,11 @@ class BlocsHandler {
   final List<BlocProvider> blocProviders = [
     BlocProvider<AuthBloc>(
       create: (ctx) => AuthBloc(
+        authRepository: ctx.read<AuthRepository>(),
+      ),
+    ),
+    BlocProvider<SigninCubit>(
+      create: (ctx) => SigninCubit(
         authRepository: ctx.read<AuthRepository>(),
       ),
     ),
